7a249e4b967850a11339916139585962a7997b49
rolanddosa <dosaroland@yahoo.com>
2019-04-07 13:44:15 +0100

    develop: Removed templates and WEB-INF directories, static files are only needed in static directory and Spring Boot will serve that index.html file

--- src/main/java/mainPackage/Application.java
+++ src/main/java/mainPackage/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.stream.Stream;
 
+@Configuration
 @SpringBootApplication
 public class Application {
 
