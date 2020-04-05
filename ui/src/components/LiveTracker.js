2a7cc02b1cd9c05275c068249e47a272fb54be7c
rolanddosa <dosaroland@yahoo.com>
2020-04-05 21:37:28 +0100

    React JSON parsing checkpoint

--- ui/src/components/LiveTracker.js
+++ ui/src/components/LiveTracker.js
@@ -1,42 +1,43 @@
 import * as React from "react";
 
 export class LiveTracker extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        // fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('http://localhost:8080/api/elexon')
+            //read this https://www.golangprograms.com/display-json-data-in-reactjs.html
+            .then(res.elexonResponseBody.responseList => json)
+            .then(json => {
+                this.setState({isLoaded: true, items: json,})
+            })
+            .catch(console.log)
+    }
+
     render() {
-        return (
-            < div>
-                <p/>
-                Live
-                Power
-                Generation
-                Tracker - coming
-                soon
-                <p/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-            </div>
-        )
+
+        var {isLoaded, items} = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>
+        } else {
+            return (
+                <div className="LiveTracker">
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.fuelType}>
+                                Name: {item.recordType} | Email: {item.fuelType}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
     }
 }
