3d10ffc86cd9c703c395d20c920a4121c025ead0
rolanddosa <dosaroland@yahoo.com>
2020-04-06 20:43:59 +0100

    React JSON parsing checkpoint

--- ui/src/components/LiveTracker.js
+++ ui/src/components/LiveTracker.js
@@ -11,10 +11,8 @@ export class LiveTracker extends React.Component {
     }
 
     componentDidMount() {
-        // fetch('http://jsonplaceholder.typicode.com/users')
         fetch('http://localhost:8080/api/elexon')
-            //read this https://www.golangprograms.com/display-json-data-in-reactjs.html
-            .then(res.elexonResponseBody.responseList => json)
+            .then(response => response.json())
             .then(json => {
                 this.setState({isLoaded: true, items: json,})
             })
@@ -22,22 +20,27 @@ export class LiveTracker extends React.Component {
     }
 
     render() {
-
-        var {isLoaded, items} = this.state;
+        let isLoaded, items;
+        ({isLoaded, items} = this.state);
         if (!isLoaded) {
             return <div>Loading...</div>
         } else {
+            console.log(items.elexonResponseBody.responseList.items);
+            const responseBody = items.elexonResponseBody;
+            console.log(responseBody);
             return (
                 <div className="LiveTracker">
                     <ul>
-                        {items.map(item => (
-                            <li key={item.fuelType}>
-                                Name: {item.recordType} | Email: {item.fuelType}
-                            </li>
-                        ))}
+                        {/*//implement https://observablehq.com/@d3/sortable-bar-chart*/}
+                    {items.elexonResponseBody.responseList.items.map((item, index) => (
+                    <li key={index}>{item.fuelType} - amount in MW - {item.currentMW}</li>
+                    ))};
                     </ul>
+                    a
                 </div>
             );
         }
     }
 }
+
+
