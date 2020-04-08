b3caadb9b0b4dd70fcb67ec63aa5571eac4d61d9
rolanddosa <dosaroland@yahoo.com>
2020-04-09 00:18:03 +0100

    Live react-chartjs-2 working with React!

--- ui/src/components/LiveTracker.js
+++ ui/src/components/LiveTracker.js
@@ -1,23 +1,94 @@
-import * as React from "react";
+import React, {useRef, useEffect, useState} from "react";
+import {select, axisBottom, axisRight, scaleLinear, scaleBand} from "d3";
+import "./LiveTracker.css";
+import rd3 from 'react-d3-library'
+import {Bar} from "react-chartjs-2";
+
+// https://www.createwithdata.com/react-chartjs-dashboard/
+// https://github.com/jerairrest/react-chartjs-2/blob/master/example/src/components/bar.js
 
 export class LiveTracker extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         items: [],
+    //         isLoaded: false,
+    //     };
+    // };
+    //
+    // componentDidMount() {
+    //     fetch('http://localhost:8080/api/elexon')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             this.setState({
+    //                 items: json,
+    //                 isLoaded: true,
+    //             })
+    //         })
+    //     // .catch(console.log)
+    // }
+    //
+    //
+    // render() {
+    //     let isLoaded, items;
+    //     ({isLoaded, items} = this.state);
+    //     if (!isLoaded) {
+    //         return <div>Loading...</div>
+    //     } else {
+    //         let labels = items.elexonResponseBody.responseList.items.map(function (e) {
+    //             return e.fuelType;
+    //         });
+    //         // console.log(labels);
+    //
+    //         let mydata = items.elexonResponseBody.responseList.items.map(function (e) {
+    //             return e.currentMW;
+    //         });
+    //         // console.log(data);
+    //
+    //         let data = [];
+    //         for (let i = 0; i < 15; i++) {
+    //             data.push({
+    //                 // label: labels[i],
+    //                 label: "a",
+    //                 value: i
+    //                 // value: mydata[i]
+    //             })
+    //         }
+    //         console.log(data);
+    //         return (
+    //             <div className="sub chart-wrapper">
+    //                 BarChart here:
+    //                 <BarChart
+    //                     data={data}
+    //                     title={"Power Chart"}
+    //                     color="#70CAD1"
+    //                 />
+    //             </div>
+    //         );
+    //     }
+    // }
 
     constructor(props) {
         super(props);
         this.state = {
             items: [],
             isLoaded: false,
-        }
-    }
+        };
+    };
 
     componentDidMount() {
         fetch('http://localhost:8080/api/elexon')
             .then(response => response.json())
             .then(json => {
-                this.setState({isLoaded: true, items: json,})
+                this.setState({
+                    items: json,
+                    isLoaded: true,
+                })
             })
-            .catch(console.log)
+        // .catch(console.log)
     }
+
 
     render() {
         let isLoaded, items;
@@ -25,22 +96,48 @@ export class LiveTracker extends React.Component {
         if (!isLoaded) {
             return <div>Loading...</div>
         } else {
-            console.log(items.elexonResponseBody.responseList.items);
-            const responseBody = items.elexonResponseBody;
-            console.log(responseBody);
+            let mylabels = items.elexonResponseBody.responseList.items.map(function (e) {
+                return e.fuelType;
+            });
+            // console.log(labels);
+
+            let mydata = items.elexonResponseBody.responseList.items.map(function (e) {
+                return e.currentMW;
+            });
+            // console.log(data);
+
+            const data = {
+                labels: mylabels,
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: mydata
+                    }
+                ]
+            };
+
             return (
-                <div className="LiveTracker">
-                    <ul>
-                        {/*//implement https://observablehq.com/@d3/sortable-bar-chart*/}
-                    {items.elexonResponseBody.responseList.items.map((item, index) => (
-                    <li key={index}>{item.fuelType} - amount in MW - {item.currentMW}</li>
-                    ))};
-                    </ul>
-                    a
+                <div>
+                    BarChart here:
+                    <Bar
+                        data={data}
+                        width={100}
+                        height={50}
+                        options={{
+                            maintainAspectRatio: false
+                        }}
+                    />
                 </div>
             );
         }
     }
+
+
 }
 
 
