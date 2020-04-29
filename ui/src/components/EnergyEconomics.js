82caf28cd58293b71128b75fa2c7a1e222f8626f
rolanddosa <dosaroland@yahoo.com>
2020-04-29 21:37:07 +0100

    Refreshed logo!

--- ui/src/components/EnergyEconomics.js
+++ ui/src/components/EnergyEconomics.js
@@ -4,7 +4,9 @@ export class EnergyEconomics extends React.Component {
     render() {
         return (
             <div className="my-5 ml-custom mr-custom">
-                <h3>The Economics</h3><br/>
+                <h3>The Economics</h3>
+                <br/>
+                <br/>
                 <h5>Government and Policy</h5>
                 <p>Stable policies play the key role of attracting investments. Unstable policies
                     with frequent changes are undesired.<br/>
@@ -12,14 +14,18 @@ export class EnergyEconomics extends React.Component {
                 <h5>Green Banks</h5>
                 <p>Entities that will act as intermediaries in handling financing. They bring expertise and specialise
                     in the sector,
-                    providing confidence to investors.<br/>
-                </p><br/>
+                    providing confidence to investors.
+                </p>
+                <br/>
+                <br/>
                 <h5>Private Investments</h5>
                 <p>Most of the funding in renewable energy comes from the private sector. Individuals and companies
                     are increasingly incorporating environmentally friendly features in their missions (i.e. Google's <a
                         href="https://sustainability.google/projects/announcement-100/"
                         target="_blank">commitment</a> to running their operations on 100% renewable energy).<br/>
-                </p><br/>
+                </p>
+                <br/>
+                <br/>
                 <h5><a>Crowdfunding Platforms</a></h5>
                 <p>Crowdfunding has two main advantages:</p>
                 <ul>
@@ -33,6 +39,12 @@ export class EnergyEconomics extends React.Component {
                     href="http://www.crowdfundres.eu/index.html@p=4.html"
                     target="_blank">CROWDFUNDRES</a> project,
                     with funding from the European Union Horizon 2020 research and innovation programme.</p>
+                <br/>
+                <br/>
+                <h5><a>Job Creation</a></h5>
+                <p>"Compared with fossil fuel technologies, which are typically mechanized and capital intensive, the renewable energy industry is more labor intensive. Solar panels need humans to install them; wind farms need technicians for maintenance.
+
+                    This means that, on average, more jobs are created for each unit of electricity generated from renewable sources than from fossil fuels." UCSUSA.org</p>
             </div>
         )
     }
